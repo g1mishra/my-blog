a2aa5a3eea512513e5ecacd2503a9dc9a74d9ee0
Jeevan <g1mishra.dev@gmail.com>
2024-01-22 22:52:13 +0530

    Update user authentication logic

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -5,13 +5,13 @@ const About = () => {
     <div className="container mx-auto p-4 text-white">
       <h1>About My Blog</h1>
       <p>
-        Welcome to my blog! I'm thrilled to have you here, and I appreciate you
+        Welcome to my blog! I&apos;m thrilled to have you here, and I appreciate you
         taking the time to learn a bit more about what this space is all about.
-        This blog is more than just a collection of words; it's a platform where
+        This blog is more than just a collection of words; it&apos;s a platform where
         I express my thoughts, share experiences, and delve into various topics
         that pique my curiosity. As a passionate writer and avid explorer of
         ideas, I aim to create a diverse and engaging environment for readers.
-        You'll find a mix of articles covering a wide spectrum, ranging from
+        You&apos;ll find a mix of articles covering a wide spectrum, ranging from
         technology and science to personal development and lifestyle. I believe
         in the power of words to inspire, educate, and connect people, and this
         blog is my canvas to contribute to that endeavor. What sets this blog
@@ -20,13 +20,13 @@ const About = () => {
         learned along the way. Life is a journey, and through my writing, I hope
         to create a sense of camaraderie with readers who may resonate with
         similar experiences or find inspiration in the narratives shared.
-        Whether you're a seasoned reader or a newcomer, I encourage you to
+        Whether you&apos;re a seasoned reader or a newcomer, I encourage you to
         explore the diverse content available. Feel free to share your thoughts
         in the comments section or reach out through the provided contact
         channels. Your feedback is invaluable and contributes to the vibrant
         community that is constantly evolving around this blog. Thank you for
-        being a part of this journey. Together, let's explore, learn, and grow
-        as we navigate the intriguing landscape of ideas and experiences. Here's
+        being a part of this journey. Together, let&apos;s explore, learn, and grow
+        as we navigate the intriguing landscape of ideas and experiences. Here&apos;s
         to meaningful connections and a shared appreciation for the beauty of
         language and storytelling.
       </p>
--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -6,10 +6,10 @@ const ContactUs = () => {
       <h1>Contact Us</h1>
       <p>
         We appreciate your interest in reaching out to us. Whether you have
-        questions, feedback, or just want to say hello, we'd love to hear from
+        questions, feedback, or just want to say hello, we&apos;d love to hear from
         you. Your thoughts and opinions matter to us, and we are committed to
         providing a responsive and open channel of communication. Feel free to
-        use the contact form below to send us a message. We'll do our best to
+        use the contact form below to send us a message. We&apos;ll do our best to
         get back to you as soon as possible. Your inquiries are important to us,
         and we value the opportunity to connect with our readers and community.
         <br /> <br /> Additionally, you can connect with us on our social media
@@ -17,7 +17,7 @@ const ContactUs = () => {
         glimpses. Follow us on [Social Media Links] to stay in the loop and
         engage with our growing community. Thank you for considering reaching
         out to us. Your engagement is a crucial part of what makes this blog
-        thrive, and we look forward to hearing from you. Together, let's
+        thrive, and we look forward to hearing from you. Together, let&apos;s
         continue building a positive and interactive space where ideas can be
         shared and discussions can flourish.
       </p>
